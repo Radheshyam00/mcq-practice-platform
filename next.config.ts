eee6c47ce7b2f433dab265973b1f4a3dc186ac01
Radheshyam00 <radheshyamjanwa666@gmail.com>
2026-09-15 09:29:10 +0530

    add 1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  allowedDevOrigins: [
+    "192.168.252.1",
+    "localhost",
+    "127.0.0.1",
+  ],
 };
 
 export default nextConfig;
